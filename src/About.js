9cfe5ebbacea17809b486f741617a9c4ddd42a16
Haree <haree.se@gmail.com>
2024-06-14 22:34:38 +0530

    react example site

---
+++ src/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <main className="About">
+      <h2>About</h2>
+      <p style={{ marginTop: "1rem" }}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+    </main>
+  )
+}
+
+export default About
---
+++ src/NewPost.js
@@ -0,0 +1,30 @@
+const NewPost = ({
+  handleSubmit, postTitle, setPostTitle, postBody, setPostBody
+}) => {
+  return (
+    <main className="NewPost">
+      <h2>New Post</h2>
+      <form className="newPostForm" onSubmit={handleSubmit}>
+        <label htmlFor="postTitle">Title:</label>
+        <input
+          type="text"
+          value={postTitle}
+          id="postTitle"
+          required
+          onChange={(e) => setPostTitle(e.target.value)} 
+        />
+        <label htmlFor="postBody">Post</label>
+        <textarea
+          id="postBody"
+          required
+          value={postBody}
+          onChange={(e) => setPostBody(e.target.value)}
+        >
+        </textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </main>
+  )
+}
+
+export default NewPost
